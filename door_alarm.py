8b62b7ea5d40d986dd6fff4be2b5a10a40fc3391
Meric Bal <ozkan.meric.bal@gmail.com>
2017-09-03 18:28:45 -0700

    Looks good.

--- door_alarm.py
+++ door_alarm.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 from twilio.rest import Client
-from credentials import sid, token, meric, my_twilio
+from credentials import sid, token, meric, nil,  my_twilio
 
 client = Client(sid, token)
 warning = 'Door has opened!'
@@ -11,15 +11,17 @@ GPIO.setmode(GPIO.BOARD)
 
 red = 11
 green = 13
+blue = 15
 
 GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
+GPIO.setup(blue, GPIO.OUT)
 
 def sensor(pin):
 	reading = 0
 	GPIO.setup(pin, GPIO.OUT)
 	GPIO.output(pin, GPIO.LOW)
-	time.sleep(1)
+	time.sleep(0.7)
 	
 	GPIO.setup(pin, GPIO.IN)
 	
@@ -36,19 +38,26 @@ while True:
 			GPIO.output(red, True)
 			time.sleep(0.2)
 			GPIO.output(red, False)
-			GPIO.output(green, True)
+			GPIO.output(blue, True)
 			time.sleep(0.2)
-			GPIO.output(green, False)
-			
+			GPIO.output(blue, False)
+
 		if sensor(7) < 130:
-#			message = client.messages.create(to=meric,
-#							from_=my_twilio,
-#							body=warning)
+			GPIO.output(green, GPIO.HIGH)
+			time.sleep(2)
+			message = client.messages.create(to=meric,
+							from_=my_twilio,
+							body=warning)
 			print 'Message sent to %r !' % meric
+			message2 = client.messages.create(to=nil,
+							from_=my_twilio,
+							body=warning)
+			print 'Message sent to %r !' % nil
 			print 'Returning to reading !'
+			GPIO.output(green, GPIO.LOW)
 
 		else:
-			print 'Returning to reading !'
+			print 'Detection stopped ! Returning to reading !'
 
 
 GPIO.cleanup()
